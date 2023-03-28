0056c8472645e7bce9f915d47895043ba57991d8
Ravi Patel <44921078+impatelravi@users.noreply.github.com>
2023-03-28 12:04:50 +0530

    Add files via upload

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,42 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<esp_provisioning/EspProvisioningPlugin.h>)
+#import <esp_provisioning/EspProvisioningPlugin.h>
+#else
+@import esp_provisioning;
+#endif
+
+#if __has_include(<flutter_ble_lib/FlutterBleLibPlugin.h>)
+#import <flutter_ble_lib/FlutterBleLibPlugin.h>
+#else
+@import flutter_ble_lib;
+#endif
+
+#if __has_include(<location/LocationPlugin.h>)
+#import <location/LocationPlugin.h>
+#else
+@import location;
+#endif
+
+#if __has_include(<permission_handler/PermissionHandlerPlugin.h>)
+#import <permission_handler/PermissionHandlerPlugin.h>
+#else
+@import permission_handler;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [EspProvisioningPlugin registerWithRegistrar:[registry registrarForPlugin:@"EspProvisioningPlugin"]];
+  [FlutterBleLibPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBleLibPlugin"]];
+  [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+}
+
+@end
